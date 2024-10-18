541f0893331784b38b0f00c87dde0267dd3d8a5d
senzesss <vischenkov1@gmail.com>
2024-10-18 17:48:42 +0300

    Я сделал Обработка исключений

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-number = int(input()) # Ввод двухзначного числа
+number = input("Введите число >> ") # Ввод двухзначного числа
 
+while type(number) != int: # обработка исключений
+    try:
+        number = int(number)
+    except ValueError:
+        print("Неправильно ввели!")
+        number = input("Введите первое число: ")
 # Поиск десятков и единиц
 des = number // 10
 ost = number % 10
