124ca9512be6f601b29c1c62e65a76be9c4d983d
senzesss <136167998+senzesss@users.noreply.github.com>
2024-11-15 00:53:38 +0300

    Update practice5.1.py

--- practice5/practice5.1.py
+++ practice5/practice5.1.py
@@ -1,1 +1,8 @@
+# Составить функцию, которая напечатает сорок любых символов
 
+import random
+def aboba():
+    for i in range(40):
+        number = random.randint(0, 100)  # значение от 0.0 до 100.0
+        print(number)
+aboba()
